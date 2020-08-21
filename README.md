e8143e1359f7415da920ae898e353eccfdfc7c77
Artem Godin <fluorumlabs@users.noreply.github.com>
2020-08-21 20:12:01 +0300

    Initial implementation

--- README.md
+++ README.md
@@ -1,27 +1,67 @@
-# TSDX Bootstrap
+# CSS Variable Observer
 
-This project was bootstrapped with [TSDX](https://github.com/jaredpalmer/tsdx).
+## Problem
+
+While CSS variables (a.k.a. [CSS custom properties](https://developer.mozilla.org/en-US/docs/Web/CSS/Using_CSS_custom_properties) are 
+very powerful, their integration with JavaScript is very limited: you can set it with `element.style.setProperty()`
+or retrieve expanded value with `getComputedStyle(element).getProperty()`. Moreover, if you add `calc()` to the mix
+(why else would you use CSS variables otherwise), you'll quickly notice that `getProperty()` doesn't returns computed
+value, but an expanded formula.
+
+Not to mention that it's not possible to observe CSS variable from the JavaScript side.
+
+## Solution
+
+This tiny library addresses both problems. It allows you to set up an observer that will track specified
+CSS variables and retrieve their final computed values.
+
+There is a catch though: only unitless numeric values (like `0.4`) are supported. It won't work with colors or  
+percentages. 
+
+## Usage
+
+`npm install css-variable-observer --save`
+
+### Vanilla JS (CommonJS)
+```js
+const CSSVariableObserver = require('css-variable-observer');
+
+const cssVariableObserver = new CSSVariableObserver(['--variable1', '--variable2'],
+    (variables) => console.log(variables['--variable1'], variables['--variable2']));
+cssVariableObserver.attach(document.body);
+```
+
+### Vanilla JS (ES6)
+```ecmascript 6
+import CSSVariableObserver from 'css-variable-observer';
+
+const cssVariableObserver = new CSSVariableObserver(['--variable1', '--variable2'],
+    (variables) => console.log(variables['--variable1'], variables['--variable2']));
+cssVariableObserver.attach(document.body);
+```
+
+### TypeScript
+```typescript
+import CSSVariableObserver from `css-variable-observer/src/index.ts`
+
+const cssVariableObserver = new CSSVariableObserver(['--variable1', '--variable2'],
+    (variables) => console.log(variables['--variable1'], variables['--variable2']));
+cssVariableObserver.attach(document.body);
+```
 
 ## Local Development
 
 Below is a list of commands you will probably find useful.
 
-### `npm start` or `yarn start`
+### `npm run demo`
 
-Runs the project in development/watch mode. Your project will be rebuilt upon changes. TSDX has a special logger for you convenience. Error messages are pretty printed and formatted for compatibility VS Code's Problems tab.
+Runs the project in development/watch mode, starts HTTP server and navigates to `http://localhost:8080/demo` 
 
-<img src="https://user-images.githubusercontent.com/4060187/52168303-574d3a00-26f6-11e9-9f3b-71dbec9ebfcb.gif" width="600" />
+### `npm start`
 
-Your library will be rebuilt if you make edits.
+Runs the project in development/watch mode. Your project will be rebuilt upon changes. 
 
-### `npm run build` or `yarn build`
+### `npm run build`
 
 Bundles the package to the `dist` folder.
 The package is optimized and bundled with Rollup into multiple formats (CommonJS, UMD, and ES Module).
-
-<img src="https://user-images.githubusercontent.com/4060187/52168322-a98e5b00-26f6-11e9-8cf6-222d716b75ef.gif" width="600" />
-
-### `npm test` or `yarn test`
-
-Runs the test watcher (Jest) in an interactive mode.
-By default, runs tests related to files changed since the last commit.
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,187 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
+/**
+ * Structure holding CSS variable values. Example:
+ *
+ * ```json
+ * {
+ *     "--my-variable": 1.0,
+ *     "--my-other-variable": 2.0
+ * }
+ * ```
+ */
+export type CssVariables = { [key: string]: number };
+
+/**
+ * Type signature of observer callback.
+ *
+ * @param values Readonly structure containing observed CSS variables and their values.
+ */
+export type CssVariableObserverCallback = (
+  values: Readonly<CssVariables>
+) => void;
+
+/**
+ * Passive observer for numeric CSS variables. Instead of typical polling approach, it uses CSS
+ * transitions to detect changes.
+ *
+ * CSSVariableObserver can be used to build dynamic theming system, detect media etc.
+ *
+ * Usage:
+ * ```javascript
+ * const cssVariableObserver = new CSSVariableObserver(['--my-variable'], (variables) => console.log("Value:",variables['--my-variable']));
+ * cssVariableObserver.attach(document.body);
+ * ```
+ *
+ * Note: Only unitless numeric values are supported. If any of observed variables have non-numeric value,
+ * the callback won't be invoked. The good news is: once the bad value is fixed, the observer will start
+ * to work again.
+ */
+export class CSSVariableObserver {
+  /**
+   * Create a new (detached) instance of CSS variable observer.
+   *
+   * @param observedVariables list of CSS variables to observe
+   * @param callback callback that will be invoked every time any of listed CSS variables change
+   */
+  constructor(
+    observedVariables: string[],
+    callback: CssVariableObserverCallback
+  ) {
+    this._observedVariables = {};
+    observedVariables.forEach((value, index) => {
+      this._observedVariables[index.toString(36).padStart(4, '!')] = value;
+    });
+    this._callback = callback;
+    this._sensor = this._createSensor();
   }
-  return a + b;
-};
+
+  /**
+   * Attach observer to target element. Values of CSS variables will be scoped to this element.
+   *
+   * Callback will be invoked immediately with the current assigne values.
+   *
+   * @param targetElement target element
+   */
+  attach(targetElement: HTMLElement): void {
+    if (!this._attached) {
+      this._sensor.addEventListener('transitionstart', this._eventHandler);
+      targetElement.appendChild(this._sensor);
+      this._attached = true;
+
+      this._handleUpdate();
+    }
+  }
+
+  /**
+   * Detach observer.
+   */
+  detach(): void {
+    if (this._attached) {
+      this._sensor.removeEventListener('transitionstart', this._eventHandler);
+      if (this._sensor.parentElement) {
+        this._sensor.parentElement.removeChild(this._sensor);
+      }
+      this._attached = false;
+    }
+  }
+
+  /**
+   * Actual element that senses when values of CSS variables are changed.
+   * @private
+   */
+  private _sensor: HTMLElement;
+
+  /**
+   * Observer CSS variables and their iternal identifiers.
+   * @private
+   */
+  private _observedVariables: Record<string, string>;
+
+  /**
+   * User supplied callback that receives CSS variable values.
+   * @private
+   */
+  private _callback: CssVariableObserverCallback;
+
+  /**
+   * Event handler that is used to invoke callback.
+   * @private
+   */
+  private _eventHandler = this._handleUpdate.bind(this);
+
+  /**
+   * Observer state
+   * @private
+   */
+  private _attached = false;
+
+  /**
+   * Setup observer sensor.
+   *
+   * In order to detect changes, `font-variation-settings` property is used. This property accepts
+   * list of variation identifiers and their numeric values, which allows to observe any number of
+   * CSS variables.
+   *
+   * @private
+   */
+  private _createSensor(): HTMLElement {
+    const sensor = document.createElement('div');
+    sensor.style.cssText =
+      'position: absolute; ' +
+      'width: 0; ' +
+      'height: 0; ' +
+      'overflow: hidden; ' +
+      'z-index: -1; ' +
+      'visibility: hidden; ' +
+      'transition: font-variation-settings 0.001ms step-start; ' +
+      'font-variation-settings: ' +
+      Object.entries(this._observedVariables)
+        .map(value => `"${value[0]}" var(${value[1]}, 0)`)
+        .join(', ');
+    return sensor;
+  }
+
+  /**
+   * Collect CSS variable values and invoke callback.
+   * @private
+   */
+  private _handleUpdate(): void {
+    if (this._attached) {
+      const computedStyle = getComputedStyle(this._sensor);
+      const fontVariationSettings = computedStyle.fontVariationSettings;
+
+      // Check if font-variation-settings are actually present in computed styles
+      if (fontVariationSettings !== undefined) {
+        // This regexp extracts identifier and number from the value of font-variation-settings property
+        const valueRegexp = /"(!{0,3}[0-9a-z]+)" ([+-]?[0-9]*([.]?[0-9]+)?([eE][+-]?[0-9]+)?)/g;
+        const variables: CssVariables = {};
+
+        let match = valueRegexp.exec(fontVariationSettings);
+        while (match) {
+          const variableName = this._observedVariables[match[1]];
+          if (
+            variableName &&
+            computedStyle.getPropertyValue(variableName) !== ''
+          ) {
+            // Only if this CSS variable is defined.
+            variables[variableName] = +match[2];
+          }
+
+          match = valueRegexp.exec(fontVariationSettings);
+        }
+
+        // Do not invoke callback if no variables are defined
+        if (Object.keys(variables).length > 0) {
+          this._callback(variables);
+        }
+      }
+    }
+  }
+}
+
+declare global {
+  interface CSSStyleDeclaration {
+    fontVariationSettings?: string;
+  }
+}
+
+export default CSSVariableObserver;
